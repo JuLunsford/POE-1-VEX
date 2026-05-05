b51a9c780c9c4802814830b712cc0056c3b0d6c6
JuLunsford <swimmingcats35220@gmail.com>
2026-05-05 09:30:54 -0400

    Add files via upload

---
+++ branchExample.py
@@ -0,0 +1,12 @@
+"""
+Name:       branchExample.py
+Author(s):  Julianne Lunsford and Jasmine Ni
+Purpose:    Demonstrate GitHub branches
+Date:       5/5/2026
+"""
+
+counter = 0
+
+while(counter < 10):
+    print("Counter: " + str(counter))
+    counter += 1
